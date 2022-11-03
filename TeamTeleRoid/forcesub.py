20961ed09aee7c74ceffd074ea1f5babb4ec1174
GreyMatter's <greymattersyt@gmail.com>
2022-11-03 09:20:05 +0530

    Update forcesub.py

--- TeamTeleRoid/forcesub.py
+++ TeamTeleRoid/forcesub.py
@@ -13,7 +13,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="You Are Ban To Use Me. Contact My [ᎯℕUℛᎯᎶ](https://t.me/Am_RoBots).",
+                text="You Are Ban To Use Me. Contact My [GreyMatter's](https://t.me/GreyMatter_Owner).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -47,7 +47,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Somthing Went Wrong. Contact My [ᎯℕUℛᎯᎶ](https://t.me/Am_RoBots).",
+            text="Somthing Went Wrong. Contact My [GreyMatter's](https://t.me/GreyMatter_Owner).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
